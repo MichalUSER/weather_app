8390fcb62997e8613f20fad3dc9b067a8f345c5f
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-05-22 12:05:35 +0000

    extract wave into component

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
   import YesterdayTemp from "./components/YesterdayTemp.svelte";
   import TempButtons from "./components/TempButtons.svelte";
   import Error from "./components/Error.svelte";
+  import Wave from "./components/Wave.svelte";
 
   import request from "./utils/request";
   import type ITemp from "./utils/itemp";
@@ -45,20 +46,7 @@
   </main>
 </div>
 
-<!-- used https://www.shapedivider.app/ -->
-<div class="wave">
-  <svg
-    data-name="Layer 1"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1200 120"
-    preserveAspectRatio="none"
-  >
-    <path
-      d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
-      class="shape-fill"
-    />
-  </svg>
-</div>
+<Wave />
 
 <svelte:head>
   {#if $visible}
@@ -84,26 +72,6 @@
   .wrapper {
     display: flex;
     justify-content: center;
-  }
-
-  .wave {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    overflow: hidden;
-    line-height: 0;
-  }
-
-  .wave svg {
-    position: relative;
-    display: block;
-    width: calc(100% + 1.3px);
-    height: 68px;
-  }
-
-  .wave .shape-fill {
-    fill: #4985a7;
   }
 
   main {
